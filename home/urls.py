6a9a31b600be026e6ee837b85a2d810b6fe242ea
Sam <138595608+AIWizard47@users.noreply.github.com>
2025-05-28 13:38:50 +0530

    Update urls.py

--- home/urls.py
+++ home/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
     path('logout/',views.log_out,name="logout"),
     path('register/', views.register_view, name='register'),
     path("market/",views.market,name="market"),
-    path('ai-guidance/', views.farmer_guidance, name='farmer_guidance'),
+    path('guidance/', views.farmer_guidance, name='farmer_guidance'),
     path('cart/add/<int:item_id>/', views.add_to_cart, name='add_to_cart'),
     path('cart/', views.view_cart, name='view_cart'),
     path('cart/remove/<int:cart_id>/', views.remove_from_cart, name='remove_from_cart'),
@@ -15,6 +15,6 @@ urlpatterns = [
     path('plants/search/', views.plant_search, name='plant_search'),
     path('payment_success/',views.payment_success,name="payment_success"),
     path('feedback/',views.feedback,name="feedback"),
-    path('guidance/',views.guidance,name="guidance"),
+    path('ai-guidance/',views.guidance,name="guidance"),
     path('search/', views.search_products, name='search_products'),
 ]
